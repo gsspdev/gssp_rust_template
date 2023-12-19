1b81c696f5064f697f266fbc6374b5cf62c5175e
gsspdev <dev@gssp.io>
2023-12-19 08:13:51 -0800

    added main() functions

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
-use regex::Regex;
+// use regex::Regex;
 
 fn main() {
-    let re = Regex::new(r"^\d{4}-\d{2}-\d{2}$").unwrap();
-    println!("Did our date match? {}", re.is_match("2014-01-01"));
+    println!("Hello, world!");
 }
